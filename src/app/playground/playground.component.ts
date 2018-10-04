0bc139fc6172274d0487e5a4b0075766b4bbefa3
Dariusz Franczak <darius32@op.pl>
2018-10-04 23:39:02 +0100

    completed

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -9,8 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class PlaygroundComponent implements OnInit {
 
   title = 'Playground';
-  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  dogs = [{ name: 'Morgan', age: 2 }, { name: 'Guiness', age: 4 }, { name: 'Rex', age: 6 }];
+  subtitle = 'Property binding';
+  inputText = 'Max 5 characters';
+  buttonText = 'Change';
+  maxLength = 5;
+  colorClass = 'color';
+  logoUrl = '';
+
+  isDisabled = false;
+
+  change() {
+    this.inputText = 'Max 10 characters';
+    this.buttonText = 'Changed';
+    this.maxLength = 10;
+    this.colorClass = 'color2';
+    this.logoUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+    this.isDisabled = true;
+  }
 
   ngOnInit() {
   }
